c66aa599329b0a98bcebb24bd23299395c1c8eb9
Your Name <ahmet.dikbayir@gmail.com>
2024-05-18 23:40:04 +0300

    orderItem class finished

--- src/main/java/com/tpe/ecommerce/domain/OrderItem.java
+++ src/main/java/com/tpe/ecommerce/domain/OrderItem.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class OrderItem {
@@ -12,6 +13,17 @@ public class OrderItem {
     @Setter(AccessLevel.NONE)
     private Long id;
 
+    @NotNull(message = "Please enter a valid quantity")
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @NotNull
+    private Double totalPrice;
+
+    @NotNull(message = "Please enter a product")
+    @ManyToOne
+    private Product product;
+    @NotNull
     @ManyToOne
     private Customer customer;
 }
