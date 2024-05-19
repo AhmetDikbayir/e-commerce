c9f94a6a0f4dafff249229016ecc50fc9b69b585
Your Name <ahmet.dikbayir@gmail.com>
2024-05-19 23:32:02 +0300

    customrDTO created

---
+++ src/main/java/com/tpe/ecommerce/dto/CustomerDTO.java
@@ -0,0 +1,37 @@
+package com.tpe.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+
+    @NotNull(message = "Name cannot be null!")
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, max = 50)
+    private String name;
+
+    @NotNull(message = "Lastname cannot be null!")
+    @NotEmpty(message = "Lastname cannot be empty")
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    @NotNull(message = "Email cannot be null!")
+    @NotEmpty(message = "Email cannot be empty")
+    @Size(min = 3, max = 50)
+    @Email(message = "Provide valid email")
+    private String email;
+
+    private String phone;
+}
